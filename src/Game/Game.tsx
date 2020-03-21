748c2aecd3416f390670f1cbad59399a0e7431b8
andreasonny83 <andreasonny83@gmail.com>
2020-03-21 20:35:25 +0000

    Add icons and refactor Game

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,61 +1,16 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography, AppBar, Toolbar, Grid, Button, Link } from '@material-ui/core';
-import { Tile, getTiles } from './tiles';
+import React, { memo } from 'react';
+import { Container, Grid } from '@material-ui/core';
+import { useTiles } from './useTiles';
+import { useStyles } from './styles';
 
-const useStyles = makeStyles(theme => ({
-  background: {
-    background: '#CCC',
-    height: '100%',
-  },
-  container: {
-    height: '100%',
-  },
-  tile: {
-    width: '80px',
-    height: '80px',
-  },
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-const getTile = (tiles: Tile[], posX: number, posY: number, gridX: number): Tile => {
-  const id = gridX * posY + posX;
-
-  return tiles[id];
-};
-
-export const Game = () => {
+export const Game = memo(() => {
   const gridSize = [10, 10];
   const classes = useStyles();
-  const { gameTiles, gridX, gridY } = getTiles(gridSize[0], gridSize[1]);
+  const { gameTiles, gridX, gridY, getTile } = useTiles(gridSize[0], gridSize[1]);
+
+  if (!gameTiles || !gameTiles.length) {
+    return <div className="Game">Loading...</div>;
+  }
 
   return (
     <div className={`Game ${classes.background}`}>
@@ -96,4 +51,4 @@ export const Game = () => {
       </Container>
     </div>
   );
-};
+});
---
+++ src/Game/styles.ts
@@ -0,0 +1,44 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(theme => ({
+  background: {
+    background: '#CCC',
+    height: '100%',
+  },
+  container: {
+    height: '100%',
+  },
+  tile: {
+    width: '80px',
+    height: '80px',
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
