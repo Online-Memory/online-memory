043bdeceb97e2f5bb070ca060f31fab5c9182921
andreasonny83 <andreasonny83@gmail.com>
2020-04-01 22:46:48 +0100

    Add animals template

--- packages/server/src/handler.js
+++ packages/server/src/handler.js
@@ -7,6 +7,7 @@ const gameTemplates = [
   { id: '001', name: 'Italy', tiles: 100, board: [10, 10] },
   { id: '002', name: 'Food', tiles: 100, board: [10, 10] },
   { id: '003', name: 'World', tiles: 100, board: [10, 10] },
+  { id: '004', name: 'Animals', tiles: 100, board: [10, 10] },
 ];
 
 const tilesBase = {
