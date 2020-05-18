f9a75630ad95da3171be65581944c1f25eb4a67c
Thetawaves <jessica.granziera@gmail.com>
2020-05-18 10:41:19 +0200

    About page

--- packages/client/src/About/About.tsx
+++ packages/client/src/About/About.tsx
@@ -11,67 +11,126 @@ export const About = () => {
         <Card elevation={3}>
           <CardHeader title="About" subheader="Game Contributors" />
           <CardContent>
-            <Grid container alignContent="center" direction="column" spacing={4}>
-              <Grid item>
+            <Grid container alignContent="center" spacing={0}>
+              <Grid container item xs={12}>
                 <Typography align="center" component="h2" variant="h4" gutterBottom>
                   This Game has been made possible thanks to...
                 </Typography>
               </Grid>
-
-              <Grid item>
-                <Typography component="h4" variant="h6" align="center">
-                  <Link color="inherit" href="https://github.com/Thetawaves">
-                    Jessica Granziera
-                  </Link>
-                </Typography>
-                <Typography paragraph>UI/UX Design</Typography>
-                <Typography paragraph>Graphics</Typography>
-                <Typography paragraph>QA / Testing</Typography>
-                <Typography paragraph>Beta Tester</Typography>
-                <Typography paragraph>Copywriting</Typography>
+              <Grid item xs={4} className={'${classes.cardColorB7} ${classes.cardCommonStyle}'}>
+                <div className={classes.cardInnerBorderBlack}>
+                  <Typography component="h4" variant="h6" align="center">
+                    Who are we?
+                  </Typography>
+                  <Typography paragraph>
+                    We are a bunch of friends that has enjoyed playing board games together since the dawn of their
+                    friendship. Memory has been a pillar of our gaming nights, with one particular person ALWAYS winning
+                    and the rest of us losing, but everyone always having fun in the process.
+                    <br />
+                    We now live scattered around the globe but want to keep our love for board gaming alive. That’s how
+                    we came up with the idea to create this neat little app and decided to share it with the rest of the
+                    world.
+                  </Typography>
+                </div>
               </Grid>
-
-              <Grid item>
-                <Typography component="h4" variant="h6" align="center">
-                  <Link color="inherit" href="https://www.linkedin.com/in/eleonora-baio-900352a1/">
-                    Eleonora Baio
-                  </Link>
-                </Typography>
-                <Typography paragraph>QA / Testing</Typography>
-                <Typography paragraph>Beta Tester</Typography>
+              <Grid item xs={4} className={(classes.cardColor222, classes.cardCommonStyle)}>
+                <div className={classes.cardInnerBorderWhite}>
+                  <Typography component="h4" variant="h6" align="center">
+                    <Link color="inherit" href="https://about.me/andreasonny83/">
+                      Andrea "Sonny" Zornada
+                    </Link>
+                  </Typography>
+                  <Typography paragraph>
+                    The brain and the arm. The man behind it all. When he’s not coding maniacally or tending to his
+                    furry beasts, you can find him enjoying tasty food or playing board games.. or doing both.
+                    <br />
+                    <br />
+                    Software Development
+                    <br />
+                    UI/UX Design
+                    <br />
+                    QA / Testing
+                    <br />
+                    Beta Tester
+                  </Typography>
+                </div>
               </Grid>
-
-              <Grid item>
-                <Typography component="h4" variant="h6" align="center">
-                  <Link color="inherit" href="https://github.com/liquidnero/">
-                    Lorenzo Coronica
-                  </Link>
-                </Typography>
-                <Typography paragraph>QA / Testing</Typography>
-                <Typography paragraph>Beta Tester</Typography>
+              <Grid item xs={4} className={(classes.cardColorDDD, classes.cardCommonStyle)}>
+                <div className={classes.cardInnerBorderBlack}>
+                  <Typography component="h4" variant="h6" align="center">
+                    <Link color="inherit" href="https://github.com/kamolins/">
+                      Kārlis Amoliņš
+                    </Link>
+                  </Typography>
+                  <Typography paragraph>
+                    QA / Testing
+                    <br />
+                    UX Testing
+                    <br />
+                    Beta Tester
+                  </Typography>
+                </div>
               </Grid>
+            </Grid>
 
-              <Grid item>
-                <Typography component="h4" variant="h6" align="center">
-                  <Link color="inherit" href="https://github.com/kamolins/">
-                    Kārlis Amoliņš
-                  </Link>
-                </Typography>
-                <Typography paragraph>QA / Testing</Typography>
-                <Typography paragraph>UX Testing</Typography>
-                <Typography paragraph>Beta Tester</Typography>
+            <Grid container item>
+              <Grid item xs={4} className={(classes.cardColorDDD, classes.cardCommonStyle)}>
+                <div className={classes.cardInnerBorderBlack}>
+                  <Typography component="h4" variant="h6" align="center">
+                    <Link color="inherit" href="https://github.com/Thetawaves">
+                      Jessica "La Jeena" Granziera
+                    </Link>
+                  </Typography>
+                  <Typography paragraph>
+                    The next best thing after an actual web designer and a graphic designer.. a design enthusiast of
+                    sorts.
+                    <br />
+                    <br />
+                    UI/UX Design
+                    <br />
+                    Graphics
+                    <br />
+                    QA / Testing
+                    <br />
+                    Beta Tester
+                    <br />
+                    Copywriting
+                  </Typography>
+                </div>
               </Grid>
-
-              <Grid item>
-                <Typography component="h4" variant="h6" align="center">
-                  <Link color="inherit" href="https://about.me/andreasonny83/">
-                    Andrea Sonny
-                  </Link>
-                </Typography>
-                <Typography paragraph>Software Development</Typography>
-                <Typography paragraph>UI/UX Design</Typography>
-                <Typography paragraph>QA / Testing</Typography>
-                <Typography paragraph>Beta Tester</Typography>
+              <Grid item xs={4} className={(classes.cardColor4B, classes.cardCommonStyle)}>
+                <div className={classes.cardInnerBorderWhite}>
+                  <Typography component="h4" variant="h6" align="center">
+                    <Link color="inherit" href="https://www.linkedin.com/in/eleonora-baio-900352a1/">
+                      Eleonora "L" Baio
+                    </Link>
+                  </Typography>
+                  <Typography paragraph>
+                    Critique magnifique. The lady behind the man.
+                    <br />
+                    <br />
+                    QA / Testing
+                    <br />
+                    Beta Tester
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid item xs={4} className={(classes.cardColorB7, classes.cardCommonStyle)}>
+                <div className={classes.cardInnerBorderBlack}>
+                  <Typography component="h4" variant="h6" align="center">
+                    <Link color="inherit" href="https://github.com/liquidnero/">
+                      Lorenzo "Enzo" Coronica
+                    </Link>
+                  </Typography>
+                  <Typography paragraph>
+                    Critique slightly less magnifique.
+                    <br />
+                    <br />
+                    QA / Testing
+                    <br />
+                    Beta Tester
+                  </Typography>
+                </div>
               </Grid>
             </Grid>
           </CardContent>
--- packages/client/src/About/styles.ts
+++ packages/client/src/About/styles.ts
@@ -4,4 +4,40 @@ export const useStyles = makeStyles(theme => ({
   container: {
     margin: theme.spacing(2, 0),
   },
+  cardInnerBorderWhite: {
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: theme.palette.type === 'dark' ? '#FFF' : '#000',
+    borderRadius: 10,
+    height: '100%',
+    padding: 10,
+  },
+  cardInnerBorderBlack: {
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: theme.palette.type === 'dark' ? '#000' : '#FFF',
+    borderRadius: 10,
+    height: '100%',
+    padding: 10,
+  },
+  cardCommonStyle: {
+    padding: 10,
+    minWidth: 150,
+  },
+  cardColorDDD: {
+    backgroundColor: theme.palette.type === 'dark' ? '#DDD' : '#222',
+    color: theme.palette.type === 'dark' ? '#000' : '#FFF',
+  },
+  cardColorB7: {
+    backgroundColor: theme.palette.type === 'dark' ? '#B7B7B7' : '#3B3B3B',
+    color: theme.palette.type === 'dark' ? '#000' : '#FFF',
+  },
+  cardColor222: {
+    backgroundColor: theme.palette.type === 'dark' ? '#222' : '#DDD',
+    color: theme.palette.type === 'dark' ? '#FFF' : '#000',
+  },
+  cardColor4B: {
+    backgroundColor: theme.palette.type === 'dark' ? '#4B4B4B' : '#B4B4B4',
+    color: theme.palette.type === 'dark' ? '#FFF' : '#000',
+  },
 }));
