12f5b82806a510430deaf2d1c3ad50647b044720
andreasonny83 <andreasonny83@gmail.com>
2020-05-18 10:07:41 +0100

    WIP

--- packages/client/src/Legal/CookieDeclaration.tsx
+++ packages/client/src/Legal/CookieDeclaration.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Grid, Typography } from '@material-ui/core';
 import { useStyles } from './styles';
 
 export const CookieDeclaration = () => {
   const classes = useStyles();
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://consent.cookiebot.com/4a20b373-e3d0-41c2-968d-be9a8eab8c78/cd.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+  }, []);
 
   return (
     <div className={`CookieDeclaration ${classes.container}`}>
@@ -12,13 +21,6 @@ export const CookieDeclaration = () => {
           <Typography component="h2" variant="h3" gutterBottom>
             Cookie Declaration
           </Typography>
-
-          <script
-            id="CookieDeclaration"
-            src="https://consent.cookiebot.com/e9be435d-3b73-472c-a503-01ee4fe8fd56/cd.js"
-            type="text/javascript"
-            async
-          ></script>
         </Grid>
       </Container>
     </div>
